f56027f999a50a108ac4eaa68ffeb578915e4624
Mark Jones <markhedleyjones@gmail.com>
2019-10-08 16:06:42 +0900

    Wrapped roslint helper scripts in main() function

--- roslint/roslint_cpplint.py
+++ roslint/roslint_cpplint.py
@@ -10,8 +10,14 @@ except ImportError:
     sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "src"))
     from roslint import cpplint_wrapper
 
-# Adding default flags to cpplint.
-# You can override them using global CMAKE variable ROSLINT_CPP_OPTS.
-# The index of injected argument is 1 because 0 is program's name
-sys.argv.insert(1, "--filter=-runtime/references")
-cpplint_wrapper.main()
+
+def main():
+    # Adding default flags to cpplint.
+    # You can override them using global CMAKE variable ROSLINT_CPP_OPTS.
+    # The index of injected argument is 1 because 0 is program's name
+    sys.argv.insert(1, "--filter=-runtime/references")
+    cpplint_wrapper.main()
+
+
+if __name__ == '__main__':
+    main()
